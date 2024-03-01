5b5651eeb71d346e60f251771c07eb952434ba06
Nehan-Shanuka <124347211+Nehan-Shanuka@users.noreply.github.com>
2024-03-01 22:00:56 +0530

    Implementing the hallModel of the database

---
+++ backend/models/hallModel.js
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+
+const hallSchema = new mongoose.Schema({
+    hallID: {
+        type: String,
+        required: true,
+    },
+    capacity: {
+        type: Number,
+        required: true,
+    },
+    projectorCount: {
+        type: Number,
+        required: true,
+    },
+    whiteboardAvailability: {
+        type: Boolean,
+        required: true,
+    },
+    micAndSpeaker: {
+        type: Boolean,
+        required: true,
+    },
+    type: {
+        type: String,
+        required: true,
+    },
+    status: {
+        type: Boolean,
+        required: true,
+    },
+    reservations: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref:  "Reservation",
+    }]
+});
+
+export const Hall = mongoose.model("Hall", hallSchema);

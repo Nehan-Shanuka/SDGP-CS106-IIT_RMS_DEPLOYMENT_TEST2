ebe32d46ae4573f320aa27fd1144a328ab6c94a8
Nehan-Shanuka <124347211+Nehan-Shanuka@users.noreply.github.com>
2024-03-06 02:51:22 +0530

    Implementing the api to filter halls according to the buildingID

--- backend/routes/hallRoutes.js
+++ backend/routes/hallRoutes.js
@@ -5,33 +5,51 @@ import { Building } from "../models/buildingModel.js";
 const router = express.Router();
 
 router.post("/:buildingId", async (request, response) => {
-    const buildingId = request.params.buildingId;
-    const newHall = new Hall(request.body);
+  const buildingId = request.params.buildingId;
+  const newHall = new Hall(request.body);
 
-    try {
-        const savedHall = await Hall.create(newHall);
-        await Building.findByIdAndUpdate(
-            buildingId,
-            { $push: { lectureHalls: savedHall._id } },
-            { new: true }
-        );
-        await savedHall.updateOne({ buildingID: buildingId });
+  try {
+    const savedHall = await Hall.create(newHall);
+    await Building.findByIdAndUpdate(
+      buildingId,
+      { $push: { lectureHalls: savedHall._id } },
+      { new: true }
+    );
+    await savedHall.updateOne({ buildingID: buildingId });
 
-        return response.status(201).json(savedHall);
-
-    } catch (error) {     
-        response.status(500).send({ message: error.message });
-    }
+    return response.status(201).json(savedHall);
+  } catch (error) {
+    response.status(500).send({ message: error.message });
+  }
 });
 
 // Get all halls
 router.get("/", async (request, response) => {
-    try {
-        const halls = await Hall.find();
-        return response.status(201).json(halls);
-    } catch (error) {
-        response.status(500).send({ message: error.message });
-    }
+  try {
+    const buildingShortNameOptions = (
+      await Building.find().select("buildingID")
+    ).toString();
+
+    let buildingShortName = request.query.buildingID.toUpperCase() || "All";
+
+    buildingShortName = buildingShortNameOptions.includes(buildingShortName)
+      ? buildingShortName
+      : "All";
+
+    const halls = await Hall.find(
+      buildingShortName === "All"
+        ? {}
+        : {
+            buildingID: await Building.find({
+              buildingID: buildingShortName,
+            }).select("_id"),
+          }
+    );
+
+    return response.status(201).json(halls);
+  } catch (error) {
+    response.status(500).send({ message: error.message });
+  }
 });
 
 export default router;

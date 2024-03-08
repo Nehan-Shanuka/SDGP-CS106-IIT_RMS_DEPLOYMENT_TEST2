699367f47716b2db266149e956a53791eb2a4123
Nehan-Shanuka <124347211+Nehan-Shanuka@users.noreply.github.com>
2024-03-08 18:54:46 +0530

    Implemented to render reservations from the database that are need the approval

--- frontend/src/pages/expandableReviewPage/index.jsx
+++ frontend/src/pages/expandableReviewPage/index.jsx
@@ -1,107 +1,71 @@
 /* eslint-disable no-unused-vars */
 import { Button, Card } from "@mui/material";
 import { Box } from "@mui/system";
-
-const timeSlotes = [
-  {
-    hallNumber: "7LA",
-    building: "SP",
-    moduleName: "Object Oriented Programming",
-    day: "Monday - 18/03/2024",
-    time: "08.30-10.30",
-    lecturer: "Mr. A. Smith",
-    course: "BSc Computer Science (Level 5)",
-    groups: ["CS-I", "CS-J", "CS-K", "CS-L"],
-    type: "Lecture",
-    description: "L5 | CS07, OOP | Week 8 Lecture",
-  },
-  {
-    hallNumber: "3LB",
-    building: "SP",
-    moduleName: "Object Oriented Programming",
-    day: "Monday - 18/03/2024",
-    time: "10.30-12.30",
-    lecturer: "Mr. A. Smith",
-    course: "BSc Computer Science (Level 5)",
-    groups: ["CS-I", "CS-J", "CS-K", "CS-L"],
-    type: "Tutorial",
-    description: "L5 | CS07, OOP | Week 8 Lecture",
-  },
-  {
-    hallNumber: "3LB",
-    building: "SP",
-    moduleName: "Object Oriented Programming",
-    day: "Monday - 18/03/2024",
-    time: "10.30-12.30",
-    lecturer: "Mr. A. Smith",
-    course: "BSc Computer Science (Level 5)",
-    groups: ["CS-I", "CS-J", "CS-K", "CS-L"],
-    type: "Tutorial",
-    description: "L5 | CS07, OOP | Week 8 Lecture",
-  },
-  {
-    hallNumber: "5LB",
-    building: "SP",
-    moduleName: "Object Oriented Programming",
-    day: "Monday - 18/03/2024",
-    time: "08.30-10.30",
-    lecturer: "Mr. A. Smith",
-    course: "BSc Computer Science (Level 5)",
-    groups: ["CS-I", "CS-J", "CS-K", "CS-L"],
-    type: "Lecture",
-    description: "L5 | CS07, OOP | Week 8 Lecture",
-  },
-  {
-    hallNumber: "5LB",
-    building: "SP",
-    moduleName: "Object Oriented Programming",
-    day: "Monday - 18/03/2024",
-    time: "08.30-10.30",
-    lecturer: "Mr. A. Smith",
-    course: "BSc Computer Science (Level 5)",
-    groups: ["CS-I", "CS-J", "CS-K", "CS-L"],
-    type: "Lecture",
-    description: "L5 | CS07, OOP | Week 8 Lecture",
-  },
-  {
-    hallNumber: "5LB",
-    building: "SP",
-    moduleName: "Object Oriented Programming",
-    day: "Monday - 18/03/2024",
-    time: "08.30-10.30",
-    lecturer: "Mr. A. Smith",
-    course: "BSc Computer Science (Level 5)",
-    groups: ["CS-I", "CS-J", "CS-K", "CS-L"],
-    type: "Lecture",
-    description: "L5 | CS07, OOP | Week 8 Lecture",
-  },
-  {
-    hallNumber: "5LB",
-    building: "SP",
-    moduleName: "Object Oriented Programming",
-    day: "Monday - 18/03/2024",
-    time: "08.30-10.30",
-    lecturer: "Mr. A. Smith",
-    course: "BSc Computer Science (Level 5)",
-    groups: ["CS-I", "CS-J", "CS-K", "CS-L"],
-    type: "Lecture",
-    description: "L5 | CS07, OOP | Week 8 Lecture",
-  },
-  {
-    hallNumber: "5LB",
-    building: "SP",
-    moduleName: "Object Oriented Programming",
-    day: "Monday - 18/03/2024",
-    time: "08.30-10.30",
-    lecturer: "Mr. A. Smith",
-    course: "BSc Computer Science (Level 5)",
-    groups: ["CS-I", "CS-J", "CS-K", "CS-L"],
-    type: "Lecture",
-    description: "L5 | CS07, OOP | Week 8 Lecture",
-  },
-];
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 export default function ExpandableReviewReservation() {
+  const [reservations, setReservations] = useState([]);
+  const [confirmation] = useState(false);
+  const [halls, setHalls] = useState([]);
+  const [buildings, setBuildings] = useState([]);
+  const [buildingID, setBuildingID] = useState([]);
+
+  useEffect(() => {
+    const url = `http://localhost:5555/reservations?confirmation=${confirmation}`;
+    axios
+      .get(url)
+      .then((response) => {
+        setReservations(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, [confirmation]);
+
+  useEffect(() => {
+    const url = `http://localhost:5555/halls?buildingID=${buildingID}`;
+    axios
+      .get(url)
+      .then((response) => {
+        setHalls(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, [buildingID]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:5555/buildings")
+      .then((response) => {
+        setBuildings(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  const handleConfirmationChange = (id) => {
+    axios
+      .put(`http://localhost:5555/reservations/${id}`, { confirmation: true })
+      .then((response) => {
+        axios
+          .get(
+            `http://localhost:5555/reservations?confirmation=${confirmation}`
+          )
+          .then((response) => {
+            setReservations(response.data);
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   return (
     <div className="w-full flex justify-center item-center">
       <Box
@@ -135,13 +99,13 @@ export default function ExpandableReviewReservation() {
               },
             }}
           >
-            {timeSlotes.map((timeSlot, index) => {
+            {reservations.map((reservation, index) => {
               return (
                 <Box
                   key={index}
                   sx={{
-                    backgroundColor:
-                      timeSlot.type !== null ? "#3E737A" : "#723E7A",
+                    backgroundColor: "#3E737A",
+                    // timeSlot.type !== null ? "#3E737A" : "#723E7A",
                     color: "#fff",
                     borderRadius: 5,
                     padding: 2,
@@ -152,14 +116,18 @@ export default function ExpandableReviewReservation() {
                     <div className="flex">
                       <div className="w-28 item-center">
                         <p className="pl-5 text-5xl text-center">
-                          {timeSlot.hallNumber}
+                          {halls.map((hall, index) => {
+                            if (hall._id === reservation.hallID) {
+                              return hall.hallID;
+                            }
+                          })}
                         </p>
                       </div>
 
                       <div className="relative ml-10">
                         <div>
-                          <p className="text-xl mx-0">{timeSlot.day}</p>
-                          <p className="my-1 mx-0">{timeSlot.time}</p>
+                          <p className="text-xl mx-0">{reservation.date}</p>
+                          <p className="my-1 mx-0">{reservation.time}</p>
                         </div>
                       </div>
                     </div>
@@ -167,7 +135,15 @@ export default function ExpandableReviewReservation() {
                     <div className="flex pr-5 pl-10">
                       <div className="flex justify-center w-20 h-20 rounded-full bg-stone-200 text-black">
                         <p className="flex justify-center items-center text-4xl">
-                          {timeSlot.building}
+                          {halls.map((hall, index) => {
+                            if (hall._id === reservation.hallID) {
+                              return buildings.map((building, index) => {
+                                if (building._id === hall.buildingID) {
+                                  return building.buildingID;
+                                }
+                              });
+                            }
+                          })}
                         </p>
                       </div>
                     </div>
@@ -175,30 +151,51 @@ export default function ExpandableReviewReservation() {
 
                   <div className="flex justify-between px-5 py-5">
                     <div className="bg-stone-200 px-3 py-1 rounded-3xl flex items-center">
-                      <p className="text-xl text-black">{timeSlot.lecturer}</p>
+                      <p className="text-xl text-black">
+                        {reservation.lecturer}
+                      </p>
                     </div>
                     <div className="">
                       <div className="bg-stone-200 px-3 py-1 rounded-3xl flex items-center">
-                        <p className="text-xl text-black">{timeSlot.type}</p>
+                        <p className="text-xl text-black">{reservation.type}</p>
                       </div>
                     </div>
                   </div>
 
                   <div className="flex justify-between px-5">
-                    <div className="bg-stone-200 rounded-2xl">
-                      <p className="text-black px-2">{timeSlot.description}</p>
+                    <div className="bg-stone-200 rounded-2xl w-full mr-10">
+                      <p className="text-black px-2">{reservation.subject}</p>
+                      <p className="text-black px-2">{reservation.course}</p>
+                      <p className="text-black px-2">
+                        {reservation.tutorialGroups.join("/ ")}
+                      </p>
+                      <p className="text-black px-2">
+                        {reservation.description}
+                      </p>
                     </div>
 
-                    <div className="grid">
+                    <div className="grid pl-5">
                       <Button
+                        onClick={() =>
+                          handleConfirmationChange(reservation._id)
+                        }
                         sx={{
                           backgroundColor: "#9DF06A",
+                          border: 0.5,
+                          borderColor: "#9DF06A",
                           color: "#000",
                           borderRadius: 5,
                           paddingTop: 1,
                           paddingBottom: 1,
-                          paddingLeft: 2,
-                          paddingRight: 2,
+                          paddingLeft: 3,
+                          paddingRight: 3,
+
+                          ":hover": {
+                            backgroundColor: "#3E737A",
+                            border: 0.5,
+                            borderColor: "#9DF06A",
+                            color: "#fff",
+                          },
                         }}
                       >
                         Accept
@@ -206,6 +203,8 @@ export default function ExpandableReviewReservation() {
                       <Button
                         sx={{
                           backgroundColor: "#EF6666",
+                          border: 0.5,
+                          borderColor: "#EF6666",
                           color: "#000",
                           borderRadius: 5,
                           paddingTop: 1,
@@ -213,6 +212,13 @@ export default function ExpandableReviewReservation() {
                           paddingLeft: 2,
                           paddingRight: 2,
                           marginTop: 1,
+
+                          ":hover": {
+                            backgroundColor: "#3E737A",
+                            border: 0.5,
+                            borderColor: "#EF6666",
+                            color: "#fff",
+                          },
                         }}
                       >
                         Decline

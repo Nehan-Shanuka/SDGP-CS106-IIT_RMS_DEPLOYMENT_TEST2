a3361daf993e224608de27e4e5a62572232f20b0
Nehan-Shanuka <124347211+Nehan-Shanuka@users.noreply.github.com>
2024-03-12 22:16:56 +0530

    Changed and modified with new componants and iplemented to import key values

--- frontend/src/components/ModuleSelection.jsx
+++ frontend/src/components/ModuleSelection.jsx
@@ -1,11 +1,14 @@
-import { useState } from 'react';
-import OutlinedInput from '@mui/material/OutlinedInput';
-import InputLabel from '@mui/material/InputLabel';
-import MenuItem from '@mui/material/MenuItem';
-import FormControl from '@mui/material/FormControl';
-import ListItemText from '@mui/material/ListItemText';
-import Select from '@mui/material/Select';
-import Checkbox from '@mui/material/Checkbox';
+/* eslint-disable react/prop-types */
+import { useEffect, useState } from "react";
+import OutlinedInput from "@mui/material/OutlinedInput";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import ListItemText from "@mui/material/ListItemText";
+import Select from "@mui/material/Select";
+import Checkbox from "@mui/material/Checkbox";
+import Box from "@mui/material/Box";
+import Chip from "@mui/material/Chip";
 
 const ITEM_HEIGHT = 48;
 const ITEM_PADDING_TOP = 8;
@@ -19,45 +22,54 @@ const MenuProps = {
 };
 
 const names = [
-  'Object Oriented Programming',
-  'Algorithms and Data Structures',
-  'Database Systems',
-  'Web Development',
-  'Machine Learning',
-  'Client Server Architecture',
+  "Object Oriented Programming",
+  "Algorithms and Data Structures",
+  "Database Systems",
+  "Web Development",
+  "Machine Learning",
+  "Client Server Architecture",
 ];
 
-export default function MultipleSelectCheckmarks() {
-  const [locationName, setLocationName] = useState([]);
+export default function MultipleSelectCheckmarks({ onModuleChange }) {
+  const [moduleSelect, setModuleSelect] = useState([]);
 
   const handleChange = (event) => {
     const {
       target: { value },
     } = event;
-    setLocationName(
+    setModuleSelect(
       // On autofill we get a stringified value.
-      typeof value === 'string' ? value.split(',') : value,
+      typeof value === "string" ? value.split(",") : value
     );
   };
-  console.log(locationName);
+
+  useEffect(() => {
+    onModuleChange(moduleSelect);
+  }, [moduleSelect, onModuleChange]);
 
   return (
     <div>
-      <FormControl sx={{ marginTop: 1, width: '100%' }}>
+      <FormControl sx={{ marginTop: 1, width: "100%" }}>
         <InputLabel id="demo-multiple-checkbox-label">Select Module</InputLabel>
         <Select
           labelId="demo-multiple-checkbox-label"
           id="demo-multiple-checkbox"
           multiple
-          value={locationName}
+          value={moduleSelect}
           onChange={handleChange}
           input={<OutlinedInput label="Select Module" />}
-          renderValue={(selected) => selected.join(', ')}
+          renderValue={(selected) => (
+            <Box sx={{ display: "flex", flexWrap: "wrap", gap: 0.5 }}>
+              {selected.map((value) => (
+                <Chip key={value} label={value} />
+              ))}
+            </Box>
+          )}
           MenuProps={MenuProps}
         >
           {names.map((name) => (
             <MenuItem key={name} value={name}>
-              <Checkbox checked={locationName.indexOf(name) > -1} />
+              <Checkbox checked={moduleSelect.indexOf(name) > -1} />
               <ListItemText primary={name} />
             </MenuItem>
           ))}

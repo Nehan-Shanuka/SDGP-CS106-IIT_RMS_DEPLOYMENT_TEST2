611c4453b10c594b0957e4f3f581c463898086cc
Nehan-Shanuka <124347211+Nehan-Shanuka@users.noreply.github.com>
2024-03-13 21:24:23 +0530

    Modified the design

--- frontend/src/pages/home/index.jsx
+++ frontend/src/pages/home/index.jsx
@@ -1,68 +1,93 @@
-import React from 'react';
-import Grid from '@mui/material/Grid';
-import Paper from '@mui/material/Paper';
-import Typography from '@mui/material/Typography';
-import { Link } from 'react-router-dom';
-import MyTimetable from '../myTimetable';
+/* eslint-disable react/prop-types */
+/* eslint-disable no-unused-vars */
+import React, { useEffect } from "react";
+import Grid from "@mui/material/Grid";
+import Paper from "@mui/material/Paper";
+import Typography from "@mui/material/Typography";
+import { Link } from "react-router-dom";
+import { useState } from "react";
 
-function Welcome() {
+function Welcome({ onStateChange }) {
   // Define a sample menus array
   const menus = [
-    { name: "My Timetable",
-      path: "/my-timetable"},
-    { name: "Reservation Requests",
-      path: "/reservations"},
-    { name: "Group Formation",
-      path: "/student-grouping"},
-    { name: "Group Timetable",
-    path: "/group-timetable" },
-    { name: "Planned Sessions",
-    path: "/planned-sessions"},
-    { name: "Group Details",
-    path: "/group-details"},
-    { name: "Reservation Review",
-    path: "/review-requests"}
+    { name: "My Timetable", path: "/my-timetable" },
+    { name: "Reservation Requests", path: "/reservations" },
+    { name: "Group Formation", path: "/student-grouping" },
+    { name: "Group Timetable", path: "/group-timetable" },
+    { name: "Planned Sessions", path: "/planned-sessions" },
+    { name: "Group Details", path: "/group-details" },
+    {},
+    { name: "Reservation Review", path: "/review-requests" },
   ];
 
-  function arracol(){
-    for(let i=0; i<menus.length; i++){
-      if(i !==6){
-        console.log(menus[i].name)
+  const [state, setState] = useState(true);
 
-      }else{
-        // console.log(menus[i].name)
-        const reservtationrevew = menus[i].name;
-        console.log(reservtationrevew)
-        reservtationrevewdisplay
-        
-      }
-    }
-  }
+  const handleStateChange = () => {
+    setState(false);
+  };
 
-  const reservtationrevewdisplay ={
-    display: 'center'
-  }
+  useEffect(() => {
+    onStateChange(state);
+  }, [state, onStateChange]);
+
+  const reservtationrevewdisplay = {
+    display: "center",
+  };
 
   return (
-    // arracol();
-    <div className='min-h-screen bg-green-800'>
-      <Grid container spacing={5} className="p-10 mt-10">
-        <Grid item xs={12}>
-          <Paper className="p-3">
-            <Typography variant="h1" component="div" className="mb-3 text-center bg-green-500">
-              WELCOME
-            </Typography>
-          </Paper>
-        </Grid>
+    <div className="bg-[#3E737A] h-screen pt-10 flex flex-col">
+      <Grid item xs={12}>
+        <Paper className="p-1">
+          <Typography
+            variant="h2"
+            component="div"
+            className="mb-3 text-center bg-stone-200 text-[#3E737A]"
+          >
+            WELCOME
+          </Typography>
+          <Typography
+            variant="h3"
+            component="div"
+            className="text-center bg-stone-200 text-[#3E737A]"
+          >
+            IIT RMS
+          </Typography>
+        </Paper>
+      </Grid>
+      <div className="grid grid-cols-3 gap-12 mx-10 my-10">
         {menus.map((menu, index) => (
-          <Grid item xs={4} sm={4} key={index}>
-             <Paper className={"p-[6.13%] text-center cursor-pointer"}>
-              <Link to={menu.path}><button className="ml-[-10%] p-5 rounded-[20%]">{menu.name}</button></Link>
-            </Paper>
+          <Grid
+            item
+            sm={4}
+            key={index}
+            className="p-2 transition ease-in-out delay-200 hover:scale-110 duration-300"
+          >
+            <div className="h-full">
+              <div
+                className={`${
+                  menu.name === undefined
+                    ? "bg-[#3E737A] pointer-events-none"
+                    : "bg-stone-200"
+                }
+                  text-center h-full cursor-pointer text-3xl`}
+              >
+                <Link to={menu.path}>
+                  <button
+                    className={`${
+                      menu.name === undefined
+                        ? "bg-[#3E737A] pointer-events-none"
+                        : "bg-stone-200"
+                    } p-[6.13%] w-full`}
+                    onClick={handleStateChange}
+                  >
+                    {menu.name}
+                  </button>
+                </Link>
+              </div>
+            </div>
           </Grid>
         ))}
-      </Grid>
-    
+      </div>
     </div>
   );
 }

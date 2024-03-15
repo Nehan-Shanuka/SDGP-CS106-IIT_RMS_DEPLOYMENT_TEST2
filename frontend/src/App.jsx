3cea32d03b0e0cbaebfa3f5e320396836fa5e35d
Nehan-Shanuka <124347211+Nehan-Shanuka@users.noreply.github.com>
2024-03-15 18:00:16 +0530

    Modified to take changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable no-unused-vars */
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
 import Topbar from "./pages/global/Topbar";
 import MyTimetable from "./pages/myTimetable";
@@ -11,10 +11,23 @@ import ExpandableReviewReservation from "./pages/expandableReviewPage";
 import UploadsPage from "./pages/Upload";
 import Userprofile from "./pages/UserProfile/index";
 import Home from "./pages/home/index";
+import Authenticator from "./pages/authentication";
+import SplashScreen from "./pages/splashScreen";
 
 export default function App() {
   const [isSidebar, setIsSidebar] = useState(false);
   const [isWelcome, setIsWelcome] = useState(true);
+  const [onBoardUser, setOnBoardUser] = useState();
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // Simulate loading time
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000); // Adjust the timeout value as needed
+  }, []);
 
   const handleStateChange = (state) => {
     setIsWelcome(state);
@@ -24,35 +37,52 @@ export default function App() {
     setIsSidebar(open);
   };
 
+  const handleUser = (user) => {
+    setOnBoardUser(user);
+    setIsAuthenticated(true);
+  };
+
+  console.log("in app ", isAuthenticated);
+  console.log("in app", onBoardUser);
+
   return (
     <>
-      {isWelcome && <Home onStateChange={handleStateChange} />}
-      {!isWelcome && (
-        <div
-          className={`${isWelcome && "overflow-hidden"}relative flex h-screen`}
-        >
-          <Navbar onSidebarOpen={handleOpen} />
-          <main className="w-full">
-            <Topbar />
-            <Routes>
-              <Route path="/my-timetable" element={<MyTimetable />} />
-              <Route
-                path="/reservations"
-                element={<Reservation isSidebarOpen={isSidebar} />}
-              />
-              <Route path="/planned-sessions" element={<PlannedSessions />} />
-              <Route path="/student-grouping" />
-              <Route
-                path="/review-requests"
-                element={<ExpandableReviewReservation />}
-              />
-              <Route path="/weekly-timetble" element={<WeeklyTimetable />} />
-              <Route path="/my-profile" element={<Userprofile />} />
-              <Route path="/data-upload" element={<UploadsPage />} />
-            </Routes>
-          </main>
-        </div>
-      )}
+      {loading ? <SplashScreen /> : (
+      !isAuthenticated ? (
+        <Authenticator userOnBoard={handleUser} />
+      ) : (isWelcome ? (
+        <Home onStateChange={handleStateChange} />
+      ) : (
+        !isWelcome && (
+          <div
+            className={`${
+              isWelcome && "overflow-hidden"
+            }relative flex h-screen`}
+          >
+            <Navbar onSidebarOpen={handleOpen} />
+            <main className="w-full">
+              <Topbar />
+              <Routes>
+                <Route path="/" element={<MyTimetable />} />
+                <Route path="/my-timetable" element={<MyTimetable />} />
+                <Route
+                  path="/reservations"
+                  element={<Reservation isSidebarOpen={isSidebar} />}
+                />
+                <Route path="/planned-sessions" element={<PlannedSessions />} />
+                <Route path="/student-grouping" />
+                <Route
+                  path="/review-requests"
+                  element={<ExpandableReviewReservation />}
+                />
+                <Route path="/weekly-timetble" element={<WeeklyTimetable />} />
+                <Route path="/my-profile" element={<Userprofile />} />
+                <Route path="/data-upload" element={<UploadsPage />} />
+              </Routes>
+            </main>
+          </div>
+        )
+      )))}
     </>
   );
 }

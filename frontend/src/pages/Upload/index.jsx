f756fa9ac108a68231ae9776e73a0bac29085046
Sanuth Dasanyaka <sanuthsasmin7@gmail.com>
2024-03-21 22:18:12 +0530

    add images and link imgs

--- frontend/src/pages/Upload/index.jsx
+++ frontend/src/pages/Upload/index.jsx
@@ -3,12 +3,35 @@ import UploadsPage from "../../components/UploadsPage";
 import SimpleTypewriter from "../../components/SimpleTypewriting";
 import MultiActionAreaCard from "../../components/MultiActionAreaCard";
 import { Button, Card } from "@mui/material";
+import img1 from "../../assets/time1.jpg"
+import img2 from "../../assets/student.png"
+import img3 from "../../assets/users.jpeg"
+
 
 export default function UploadPage() {
   return (
     <div>
-<SimpleTypewriter words={["Timetable File "," Hall Details ","Lecturer List","Ramakrishna Building"]} text={"Upload " }/>
-<MultiActionAreaCard/>
+<SimpleTypewriter words={["Timetables  "," Student List ","Users Details"]} text={"Upload " }/>
+<Card sx={{
+  backgroundColor: "#f0f0f0",
+  padding: "20px",
+  width: "80%", // Initial width
+  margin: "auto", // Center the card horizontally
+  '@media (max-width: 768px)': {
+    width: "90%", // Adjust width for smaller screens
+    padding: "10px" // Adjust padding for smaller screens
+  }
+}}>
+  <div style={{ display: "grid", gridTemplateColumns: "repeat(3, 1fr)", gap: "1rem" }}>
+  <MultiActionAreaCard type={"Timetable"} img={img1} />
+    <MultiActionAreaCard type={"Studets"} img={img2}/>
+    <MultiActionAreaCard type={"Users"} img={img3}  />
+  </div>
+</Card>
+
+
+
+
     </div>
 
 

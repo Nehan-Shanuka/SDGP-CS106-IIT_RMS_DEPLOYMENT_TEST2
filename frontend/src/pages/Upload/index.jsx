524f114627bf72ed6e4fc3d4643175c4e74edec5
Sanuth Dasanyaka <sanuthsasmin7@gmail.com>
2024-03-21 20:57:48 +0530

    add componets for MultiActionAreaCard

--- frontend/src/pages/Upload/index.jsx
+++ frontend/src/pages/Upload/index.jsx
@@ -1,15 +1,19 @@
 // In UploadPage.js
 import UploadsPage from "../../components/UploadsPage";
 import SimpleTypewriter from "../../components/SimpleTypewriting";
+import MultiActionAreaCard from "../../components/MultiActionAreaCard";
+import { Button, Card } from "@mui/material";
 
 export default function UploadPage() {
   return (
-    <div className="uploads-container">
-      {/* <UploadsPage /> */}
-      <SimpleTypewriter words={["Timetable File "," Hall Details ","Lecturer List","Ramakrishna Building"]} text={"Upload " }/>
-      
+    <div>
+<SimpleTypewriter words={["Timetable File "," Hall Details ","Lecturer List","Ramakrishna Building"]} text={"Upload " }/>
+<MultiActionAreaCard/>
+    </div>
+
+
     
 
-    </div>
+
   );
 };
